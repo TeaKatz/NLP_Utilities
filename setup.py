87c4a3da58eff1efb6697a5de18d3c1cb4346803
Panuthep <falcon_270394@hotmail.co.th>
2021-08-03 22:36:51 +0700

    Added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+import subprocess
+
+
+subprocess.run(["pip", "install", "--editable", "./NLP_Datasets"])
+subprocess.run(["pip", "install", "--editable", "./NLP_Losses"])
+subprocess.run(["pip", "install", "--editable", "./NLP_Metrics"])
+subprocess.run(["pip", "install", "--editable", "./NLP_Models"])
+subprocess.run(["pip", "install", "--editable", "./NLP_ModelTrainers"])
+subprocess.run(["pip", "install", "--editable", "./NLP_Preprocessors"])
