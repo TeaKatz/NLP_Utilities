605df3acac8ed098e20710785157761a7aa4c600
Panuthep <falcon_270394@hotmail.co.th>
2021-08-04 02:54:04 +0700

    Added install and uninstall

---
+++ uninstall.py
@@ -0,0 +1,9 @@
+import subprocess
+
+
+subprocess.run(["pip", "uninstall", "nlp-datasets"])
+subprocess.run(["pip", "uninstall", "nlp-losses"])
+subprocess.run(["pip", "uninstall", "nlp-metrics"])
+subprocess.run(["pip", "uninstall", "nlp-models"])
+subprocess.run(["pip", "uninstall", "nlp-modelTrainers"])
+subprocess.run(["pip", "uninstall", "nlp-preprocessors"])
